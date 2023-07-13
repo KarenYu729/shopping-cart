983a16dca24ef6d07c8232c6752228a70ed4491c
Jiahui Yu <97644710+KarenYu729@users.noreply.github.com>
2023-07-12 19:48:04 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {Component} from "react";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {Container, Navbar} from "react-bootstrap";
+import Home from "./pages/Home";
+import Store from "./pages/Store";
+import Cart from "./pages/Cart";
+import NavBarComponent from "./components/NavBar.component";
+import {ShopContextProvider} from "./context/ShopContext";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <ShopContextProvider>
+                    <NavBarComponent/>
+                    <Container className={"mb-4"}>
+                        <Routes>
+                            <Route path={"/shopping-cart/"} element={<Home/>}></Route>
+                            <Route path={"/shopping-cart/store"} element={<Store/>}></Route>
+                            <Route path={"/shopping-cart/cart"} element={<Cart/>}></Route>
+                        </Routes>
+                    </Container>
+                </ShopContextProvider>
+
+
+            </div>
+        );
+    }
+
+
 }
+
 
 export default App;
